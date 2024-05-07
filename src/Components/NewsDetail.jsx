543d6cbc826cd9f9ffa4989482f0650b2dcef903
seemapoudel58 <simapoudel6@gmail.com>
2024-05-07 22:04:07 +0545

    complete

--- src/Components/NewsDetail.jsx
+++ src/Components/NewsDetail.jsx
@@ -3,25 +3,24 @@ import { useParams } from "react-router-dom";
 import { API_KEY } from "../utilities/config";
 import axios from "axios";
 import { Link } from "react-router-dom";
+import { FaInfoCircle, FaUser, FaCalendarAlt } from "react-icons/fa";
 
 const apiUrl = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`;
 
 const NewsDetail = () => {
   const { articleUrl } = useParams();
-  console.log("Article URL:", articleUrl);
   const [article, setArticle] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [description, setDescription] = useState(false);
 
   useEffect(() => {
     setLoading(true);
     axios
       .get(apiUrl)
       .then((res) => {
-        console.log("API Response:", res.data);
         const foundArticle = res.data.articles.find(
           (article) => article.url === articleUrl
         );
-        console.log("Found Article:", foundArticle);
         setArticle(foundArticle);
       })
       .catch((err) => {
@@ -32,31 +31,52 @@ const NewsDetail = () => {
       });
   }, [articleUrl]);
 
+
+  const toggleDescription = () =>{
+    setDescription(!description);
+  }
+
   return (
-    <div>
+    <div className="container mx-auto py-8">
       {loading ? (
-        <p>Loading...</p>
+        <p className="text-center text-gray-700">Loading...</p>
       ) : article ? (
         <>
-          <div>
+          <div className="mb-6 flex justify-end ">
             <Link to={"/"}>
-              <button> Home </button>
+              <button className= "  bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                Home
+              </button>
             </Link>
           </div>
-          <div>
-            <h2>{article.title}</h2>
-            <p>{article.content}</p>
-            <p>{article.description}</p>
-            <p>Published At: {article.publishedAt}</p>
-            <p>source of news: {article.source.name}</p>
-            <p>Author: {article.author}</p>
-            <a href={article.url} target="_blank" rel="noopener noreferrer">
-              Read More
-            </a>
+          <div className="max-w-2xl mx-auto">
+            <h2 className="text-3xl font-bold mb-9">{article.title}</h2>
+            <p className="text-gray-700 mb-4">{article.content}</p>
+            <div className="flex items-center mb-4">
+              <FaInfoCircle className="text-blue-500 mr-2 cursor-pointer text-2xl" onClick={toggleDescription} />
+              <span className="text-gray-700 font-semibold">Description</span>
+            </div>
+            {description && (
+              <div className="bg-gray-100 p-4 mb-4 rounded">
+                <p className="text-gray-700">{article.description}</p>
+              </div>
+            )}
+            <div className="flex items-center text-gray-700 mb-4">
+              <FaUser className="mr-2 text-xl" />
+              <span className="font-semibold">{article.author}</span>
+            </div>
+            <div className="flex items-center text-gray-700 mb-2">
+              <FaCalendarAlt className="mr-2 text-xl" />
+              <span className="font-semibold">Published At: </span> {article.publishedAt}
+            </div>
+            <div className="text-gray-700 mt-8 mb-3">
+              <span className="font-semibold">Source of news:</span> {article.source.name}
+            </div>
+            <a href={article.url} target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:underline">Read More</a>
           </div>
         </>
       ) : (
-        <p>Article not found</p>
+        <p className="text-red-500">Article not found</p>
       )}
     </div>
   );
